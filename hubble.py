23460635fe7a6501a33d8b1268aa54fefb490734
techyguyperplexable <154611145+techyguyperplexable@users.noreply.github.com>
2025-05-27 15:02:57 +0100

    9611: a little experiment

--- hubble.py
+++ hubble.py
@@ -74,11 +74,11 @@ exynos_data = [
         "Exynos9610\0",
 
         [  # S-Boot Split Values
-            ["part1.bin", 0x0000,  0x2000],
-            ["part2.bin", 0x2000,  0x15000],
-            ["part3.bin", 0x15000, 0x44000],
-            ["part4.bin", 0x5A000, 0x5A000 + 0x180000],  # 0x5A000 to 0x1DA000
-            ["part5.bin", 0x1DA000, 0x1DA000 + 0x40000], # 0x1DA000 to 0x21A000
+            ["fwbl1.img", 0x0000,  0x2000],
+            ["epbl.img", 0x2000,  0x15000],
+            ["bl2.img", 0x15000, 0x44000],
+            ["lk.bin", 0x5A000, 0x5A000 + 0x180000],  # 0x5A000 to 0x1DA000
+            ["el3_mon.img", 0x1DA000, 0x1DA000 + 0x40000], # 0x1DA000 to 0x21A000
             ["part6.bin", 0x21A000, 0x31B000]
         ],
 
